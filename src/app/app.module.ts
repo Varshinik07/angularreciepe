db79a8ef4296d5622860dd51423e999780754cf7
Varshini <varshuk01@gmail.com>
2022-10-19 19:59:32 +0530

    viewreciepe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddreciepeComponent } from './addreciepe/addreciepe.component';
+import { ViewreciepeComponent } from './viewreciepe/viewreciepe.component';
+import{HttpClientModule} from '@angular/common/http';
 const appRoutes:Routes=[
   {
     path:"",component:AddreciepeComponent
+  },
+  {
+    path:"viewreciepe",component:ViewreciepeComponent
   }
 ]
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddreciepeComponent
+    AddreciepeComponent,
+    ViewreciepeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
